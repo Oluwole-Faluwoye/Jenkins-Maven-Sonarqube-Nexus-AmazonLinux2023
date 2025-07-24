e735e3c8bc9b11ba4e864213879c2d14a5189574
unknown <faluwoyeo@yahoo.com>
2025-07-24 13:33:57 -0700

    updated jenkinsfile

--- Archive/jenkins-maven-install.sh
+++ Archive/jenkins-maven-install.sh
@@ -29,7 +29,7 @@ mvn -v
 # Create ".m2" and download your "settings.xml" file into it to Authorize Maven
 ## Make sure to Update the RAW GITHUB Link to your "settings.xml" config
 mkdir /var/lib/jenkins/.m2
-wget https://raw.githubusercontent.com/awanmbandi/realworld-cicd-pipeline-project/refs/heads/jenkins-maven-sonarqube-nexus/settings.xml -P /var/lib/jenkins/.m2/
+wget https://raw.githubusercontent.com/Oluwole-Faluwoye/Jenkins-Maven-Sonarqube-Nexus-AmazonLinux2023/refs/heads/main/settings.xml -P /var/lib/jenkins/.m2/
 chown -R jenkins:jenkins /var/lib/jenkins/.m2/
 chown -R jenkins:jenkins /var/lib/jenkins/.m2/settings.xml
 
